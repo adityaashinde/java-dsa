535b8084ba57c4c0ef6a99d07f0faa5ef3aa1135
adityaashinde <adityashinde4580@gmail.com>
2023-05-26 13:50:59 +0530

    quizes

---
+++ Quiz/Loops Basic/CompileError.java
@@ -0,0 +1,11 @@
+class CompileError {
+	public static void main(String[]args){
+
+		int var = 10;
+		while (var >5){
+			System.out.println(var);
+			break;
+			var--;
+		}
+	}
+}
---
+++ Quiz/Loops Basic/Question4.java
@@ -0,0 +1,12 @@
+class Question4 {
+	public static void main(String[]args){
+
+		int var = 10;
+		while (var > 0 ){
+			System.out.println(var);
+			var--;
+			if(var < 5)
+				break;
+		}
+	}
+}
