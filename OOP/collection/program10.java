b6266746ec3e234a96f75559422519d354d5aba7
Aditya Shinde <adityashinde4580@gmail.com>
2023-08-17 17:04:03 +0530

    collections-class
    same same data access easily

---
+++ OOP/collection/program10.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class SortDemo{
+	public static void main(String[]args){
+
+		ArrayList al = new ArrayList();
+
+		al.add("Kanha");
+		al.add("Ashish");
+		al.add("Badhe");
+		al.add("Rahul");
+		al.add("Shashi");
+		al.add("Rahul");
+
+		System.out.println(al);
+
+		Collections.sort(al);
+
+		System.out.println(al);
+	}
+}
+
